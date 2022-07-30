b0d23669081fd9433c40a5f9756785ab78688ef4
DeokgyunSHIN <104719555+DeokgyunSHIN@users.noreply.github.com>
2022-07-31 01:16:29 +0900

    Update Ex3.java

--- Ex3.java
+++ Ex3.java
@@ -15,23 +15,23 @@ public class Ex3 {
 		int point=0;
 		System.out.print("구구단 게임을 시작하시겠습니까? yes/no>>");
 		String game=scan.next();
-		if(game.equals("yes")) {
+		if(game.equals("yes")) {   // 유저가 yes를 입력하면 게임시작!
 			while(point<100) {
 				int n=0;
-			int a=(int)(Math.random()*9)+1;
+			int a=(int)(Math.random()*9)+1;    // 램덤 함수
 			int b=(int)(Math.random()*9)+1;
 			System.out.println("문제: "+ a+" x "+b+"= ");
 			n=scan.nextInt();
-			if (n==(a*b)) {
+			if (n==(a*b)) {    //정답이면 +10
 				point+=10;
 				System.out.println("정답!!! 현재점수 :"+point);
-			}else {
+			}else {            //오답이면 -10
 				point-=10;
 				System.out.println("오답!!! 현재점수 :"+point);
 			}
 			}
-			System.out.println("100점으로 게임을 종료합니다...");
-		}else {
+			System.out.println("100점으로 게임을 종료합니다...");   // 100점을 채웠기때문에 게임 종료 
+		}else {                          //유저가 no라고 하면 그냥 게임 종료
 			System.out.println("구구단 게임을 종료합니다..");
 			System.exit(0);
 		}
